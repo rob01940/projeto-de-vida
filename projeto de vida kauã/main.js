d6e6fcd4d3643b320559cb0d1914540f84bb7180
rob01940 <132097962+rob01940@users.noreply.github.com>
2026-07-07 08:25:45 -0300

    Add files via upload

--- projeto de vida kauã/main.js
+++ projeto de vida kauã/main.js
@@ -15,10 +15,13 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2027-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2026-06-12T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-11T00:00:00");
+const tempoObjetivo3 = new Date("2026-10-01T00:00:00");
+const tempoObjetivo4 = new Date("2027-01-06T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -27,10 +30,35 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
+    
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
+    }
 }
+
+function atualizaCronometro(){
+    for (let i = 0; i<contadores.length;i++) {
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+        console.log("seg"+i);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
